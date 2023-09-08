576654508c172b0f04f862b48d8cf48eaf92df04
taha <taha@ex.com>
2023-09-07 21:53:17 -0700

    see moderator pages

---
+++ src/pages/moderator/CreateModerator.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Create from '../utils/Create';
+import BreadCrumb from '../../components/BreadCrumb/BreadCrumb';
+
+const CreateModerator = () => {
+  const fields = [
+    { name: 'first_name', label: 'First Name', type: 'text' },
+    { name: 'last_name', label: 'Last Name', type: 'text' },
+    { name: 'email', label: 'Email', type: 'email' },
+    { name: 'password', label: 'password', type: 'password' },
+    { name: 'phone_number', label: 'Mobile', type: 'number' },
+  ];
+  return (
+    <div>
+          <BreadCrumb back={'/'} place={breadcrumbItems} />
+
+      <h1 className="text-2xl">Add New Moderator</h1>
+      <Create
+        fields={fields}
+        endpoint="admin/moderators"
+        redirectPath="/subadmins"
+      />
+    </div>
+  );
+};
+
+export default CreateModerator;
