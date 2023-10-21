0e6af293a2f35b53be7267755ad82bc20bd3f058
taha <taha@dev>
2023-10-21 06:17:09 -0700

    logout

--- src/components/logout/Logout.jsx
+++ src/components/logout/Logout.jsx
@@ -1,9 +1,6 @@
-import React from 'react';
-
 const Loading = () => {
   return (
     <>
-      <h2 className="text-center text-white mt-72 text-6xl">Logging out</h2>
       <div className="loader">
         <div className="circle one"></div>
         <div className="circle two"></div>
