5e944a8f5dd29ca90aae82767f0514675e1cbba1
taha <taha@dev>
2023-10-21 06:13:51 -0700

    wrap applicatoin with authProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
+import React from 'react';
 import App from './App.jsx';
 import './index.css';
+import { AuthProvider } from './context/ContextProvider.jsx';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
